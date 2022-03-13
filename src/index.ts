3bdec683b7d9e82cbf460e12d5439867b9a41e3d
Park Changyong <tempus157@gmail.com>
2022-03-13 11:24:07 +0900

    init nodemon / ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-import { Client } from "discord.js";
+import { Client, Intents } from "discord.js";
+import { token } from "./env";
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => {
+  console.log("Ready");
+});
+
+client.login(token);
