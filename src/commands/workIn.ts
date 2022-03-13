122f36d15f08f9d915707f32b77e381827b5a1a4
Park Changyong <tempus157@gmail.com>
2022-03-13 14:13:23 +0900

    Add work out feature

--- src/commands/workIn.ts
+++ src/commands/workIn.ts
@@ -14,13 +14,14 @@ async function execute(interaction: CommandInteraction) {
       content: "이미 출근하셨습니다. 혹시 어제 퇴근하는 걸 까먹으셨나요?",
       ephemeral: true,
     });
-  } else {
-    WorkLog.create({
-      userID: interaction.user.id,
-      workInTime: interaction.createdTimestamp,
-    });
-    await interaction.reply(`${interaction.user.username}님이 출근했습니다.`);
+    return;
   }
+
+  WorkLog.create({
+    userID: interaction.user.id,
+    workInTime: interaction.createdTimestamp,
+  });
+  await interaction.reply(`${interaction.user.username}님이 출근했습니다.`);
 }
 
 export default { builder, execute } as Command;
--- src/commands/workOut.ts
+++ src/commands/workOut.ts
@@ -1,13 +1,26 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
 import { Command } from ".";
+import { WorkLog } from "../models/WorkLog";
 
 const builder = new SlashCommandBuilder()
   .setName("퇴근")
-  .setDescription("이것은 그저 퇴근");
+  .setDescription("내가 퇴근하는 걸 모두에게 알리기");
 
 async function execute(interaction: CommandInteraction) {
-  await interaction.reply(`퇴근 success`);
+  const workLog = await WorkLog.getCurrent(interaction.user.id);
+  if (!workLog) {
+    await interaction.reply({
+      content:
+        "출근도 안 하시고 퇴근하려 하시네요. 혹시 출근하는 걸 까먹으셨나요?",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  workLog.workOutTime = interaction.createdTimestamp;
+  workLog.save();
+  await interaction.reply(`${interaction.user.username}님이 퇴근하십니다...`);
 }
 
 export default { builder, execute } as Command;
